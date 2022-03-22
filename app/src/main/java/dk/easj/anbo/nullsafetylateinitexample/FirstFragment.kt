8fbb9d6a4a6cbc21adc29d8ffa1b12e2e89e2a78
Anders Børjesson <anbo@zealand.dk>
2022-03-22 21:44:44 +0100

    gradle update

--- app/src/main/java/dk/easj/anbo/nullsafetylateinitexample/FirstFragment.kt
+++ app/src/main/java/dk/easj/anbo/nullsafetylateinitexample/FirstFragment.kt
@@ -1,6 +1,7 @@
 package dk.easj.anbo.nullsafetylateinitexample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -38,9 +39,10 @@ class FirstFragment : Fragment() {
     }
 
     private fun doIt() {
-        val name: String = "Anders"
-        //var name: String = null // Null can not be a value of a non-null type
-        var length = name.length
+        //val name: String = "Anders"
+        val name: String = "kk" // Null can not be a value of a non-null type
+        val length: Int = name.length
+        Log.d("APPLE", length.toString())
 
         val input: String = binding.nameEditText.text.toString().trim()
 
@@ -57,7 +59,7 @@ class FirstFragment : Fragment() {
         // so-called Elvis operator (Elvis Presley hair style)
         // if (name2 == null) length3=-1 else length3=name2.length
 
-        val name2a: Int = name2!!.length
+        val name2a: Int? = name2?.length
         // !! convert from String? to String
         // !! programmer responsibility: Risk of NullPointerException
 
